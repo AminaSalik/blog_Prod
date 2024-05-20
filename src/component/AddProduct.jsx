72b0a35e01895ffe3b892809dd26c190da02f413
AminaSalik <aminasalik012@gmail.com>
2024-05-20 22:23:57 +0000

    modif

--- src/component/AddProduct.jsx
+++ src/component/AddProduct.jsx
@@ -49,25 +49,25 @@ function AddProduct() {
                                 <div className="p-4">
                                     <form className="space-y-4">
                                         <div>
-                                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
+                                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
                                             <input
-                                                type="email"
-                                                name="email"
-                                                id="email"
+                                                type=""
+                                                name=""
+                                                id=""
                                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                                placeholder="name@company.com"
+                                                placeholder="write..."
                                                 required
                                             />
                                         </div>
 
                                         <div>
-                                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">contents</label>
+                                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">contents</label>
                                             <input
-                                                type="email"
-                                                name="email"
-                                                id="email"
+                                                type="text"
+                                                name=""
+                                                id=""
                                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                                placeholder="name@company.com"
+                                                placeholder="write..."
                                                 required
                                             />
                                         </div>
